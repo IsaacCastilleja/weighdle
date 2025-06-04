4e683e34dda09a804157137dda2a790ee505a01c
ARatherDarkCorner <isaaccastilleja6@gmail.com>
2025-06-04 03:11:52 -0500

    previous guesses get filled

--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -1,15 +1,37 @@
 import styles from "./Game.module.css";
+import * as React from "react";
+import {useState} from "react";
+
+
+
 
 export function Guess() {
+    const [guessArray, setGuessArray] = useState(["", "", "", "", ""]);
+    const [guessCount, setGuessCount] = useState(0);
+    const [inputValue, setInputValue] = useState("");
+    function EnterGuess(e: React.ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log("EnterGuess", e);
+        setGuessCount(guessCount + 1);
+        setGuessArray(guessArray => {
+            const updatedItems = [...guessArray]
+            updatedItems[guessCount] = inputValue;
+            return updatedItems;
+        });
+        console.log(guessArray);
+    }
     return (
         <>
-            <form className={styles.GuessesContainer}>
-                <input disabled={true} type={"text"} className={styles.Guess}/>
-                <input disabled={true} type={"text"} className={styles.Guess}/>
-                <input disabled={true} type={"text"} className={styles.Guess}/>
-                <input disabled={true} type={"text"} className={styles.Guess}/>
-                <input disabled={true} type={"text"} className={styles.Guess}/>
-                <input placeholder={"Enter a guess... NOW!!!!"} type={"text"} className={styles.GuessInput}/>
+            <div className={styles.GuessesContainer}>
+                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[0] || ""}/>
+                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[1] || ""}/>
+                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[2] || ""}/>
+                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[3] || ""}/>
+                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[4] || ""}/>
+            </div>
+
+            <form className={styles.GuessesContainer} onSubmit={EnterGuess}>
+                <input placeholder={"Enter a guess... NOW!!!!"} type={"text"} className={styles.GuessInput} onChange={e => setInputValue(e.target.value)}/>
             </form>
 
         </>
