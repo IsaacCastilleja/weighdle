76937ba2eafb44aea9711fa50c6dfa3fb8e267f4
ARatherDarkCorner <isaaccastilleja6@gmail.com>
2025-06-06 11:34:02 -0500

    icon hint working on previous guesses

--- src/components/Guess.tsx
+++ src/components/Guess.tsx
@@ -1,25 +1,45 @@
 import styles from "./Game.module.css";
-import * as React from "react";
-import {useContext, useState} from "react";
+import {useContext, useState, type ChangeEvent} from "react";
 import {AnswerContext} from "../Contexts.ts";
+import {PreviousGuess} from "./PreviousGuess.tsx";
 
+interface GuessObject {
+    guessText: string;
+    arrowState: "correct" | "below" |"above";
+    colorHint: "none" | "close" | "far" | "correct";
+}
 
-
+export type { GuessObject as GuessObjectType };
 
 export function Guess() {
     const useGetAnswer = () => useContext(AnswerContext)
-    const [guessArray, setGuessArray] = useState(["", "", "", "", ""]);
     const [guessCount, setGuessCount] = useState(0);
     const [inputValue, setInputValue] = useState("");
     const [inputDisabled, setInputDisabled] = useState(false);
     const answer = useGetAnswer();
 
-    function HandleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    const defaultGuessObject: GuessObject[] = Array.from({ length: 5 }, () => ({
+        guessText: "",
+        arrowState: "correct",
+        colorHint: "none"
+    }));    const [guessObjects, setGuessObjects] = useState<GuessObject[]>(defaultGuessObject);
+
+    function checkGuess(inputValue: string) {
+        const guess: GuessObject = {
+            guessText: inputValue,
+            arrowState: "below",
+            colorHint: "far",
+        }
+        return guess;
+    }
+
+
+    function HandleSubmit(e: ChangeEvent<HTMLFormElement>) {
         e.preventDefault();
         setGuessCount(guessCount + 1);
-        setGuessArray(guessArray => {
-            const updatedItems = [...guessArray];
-            updatedItems[guessCount] = inputValue;
+        setGuessObjects(guessObjects => {
+            const updatedItems = [...guessObjects];
+            updatedItems[guessCount] = checkGuess(inputValue);
             return updatedItems;
         });
         const inputAnswer = Number.parseFloat(inputValue);
@@ -37,11 +57,12 @@ export function Guess() {
     return (
         <>
             <div className={styles.GuessesContainer}>
-                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[0] || ""}/>
-                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[1] || ""}/>
-                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[2] || ""}/>
-                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[3] || ""}/>
-                <input disabled={true} type={"text"} className={styles.Guess} value={guessArray[4] || ""}/>
+                <PreviousGuess guessObject={guessObjects[0]}/>
+                <PreviousGuess guessObject={guessObjects[1]}/>
+                <PreviousGuess guessObject={guessObjects[2]}/>
+                <PreviousGuess guessObject={guessObjects[3]}/>
+                <PreviousGuess guessObject={guessObjects[4]}/>
+
             </div>
 
             <form className={styles.GuessesContainer} onSubmit={HandleSubmit}>
---
+++ src/components/PreviousGuess.tsx
@@ -0,0 +1,51 @@
+import styles from "./PreviousGuess.module.css";
+import arrow from "../assets/arrow.svg";
+import {useState} from "react";
+import { type GuessObjectType } from "./Guess.tsx";
+
+const down_arrow = "180";
+const up_arrow = "0";
+
+const hintColorMap: Record<string, string> = {
+    "none": "",
+    "close": "yellow",
+    "far": "red",
+    "correct": "green",
+}
+
+let previousGuessObject: GuessObjectType | null = null;
+
+export function PreviousGuess(props: {guessObject: GuessObjectType}) {
+    const [arrowDirection, setArrowDirection] = useState(up_arrow);
+    const [distanceHint, setDistanceHint] = useState("");
+    const [hideImage, setHideImage] = useState(false);
+
+    if (previousGuessObject !== props.guessObject) {
+        setHideImage(props.guessObject.arrowState === "correct");
+
+        setDistanceHint(hintColorMap[props.guessObject.colorHint]);
+        setArrowDirection(props.guessObject.arrowState === "below" ? down_arrow : up_arrow);
+        previousGuessObject = props.guessObject;
+    }
+
+
+    return (
+        <>
+            <div className={styles.PreviousGuess}>
+                <input disabled={true} type={"text"} className={styles.Guess} value={props.guessObject.guessText || ""}/>
+                <div className={styles.HintContainer}>
+                    <img
+                        hidden={hideImage}
+                        src={arrow}
+                        className={styles.Hint}
+                        alt="arrow"
+                        style={{
+                            transform: `rotate(${arrowDirection}deg)`,
+                            backgroundColor: distanceHint,
+                        }}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
