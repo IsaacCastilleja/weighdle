0917508482aaea05c5e087889f894ad8e14f4239
ARatherDarkCorner <isaaccastilleja6@gmail.com>
2025-06-08 17:47:30 -0500

    Added item name to question card

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,12 +6,14 @@ export function Game(props: {question: Question}) {
     return (
         <>
             <div className={styles.GameContainer}>
-                <div className={styles.QuestionContainerContainer}>
-                    <div className={styles.QuestionContainer}>
-                        <img className={styles.QuestionImage} src={props.question.image} alt={"oops"}/>
+                <div className={styles.FullQuestion}>
+                    <div className={styles.QuestionContainerContainer}>
+                        <div className={styles.QuestionContainer}>
+                            <img className={styles.QuestionImage} src={props.question.image} alt={"oops"}/>
+                        </div>
                     </div>
+                    <p className={styles.QuestionName}>{props.question.name}</p>
                 </div>
-
                 <Guess answer={props.question.weight}/>
             </div>
         </>
