797e96e387770f3bc4e05e4ff3b12490c6d5c1d2
ARatherDarkCorner <isaaccastilleja6@gmail.com>
2025-06-08 22:50:03 -0500

    Answer now won't show up in html

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -23,16 +23,21 @@ export function Game(props: {question: Question}) {
     }
 
     function revealAnswer() {
+        setAnswerString(`The answer was ${answerWeight[inputUnit].toFixed(2)} ${inputUnit} !`);
         setAnswerHidden(false);
     }
 
     function handleGameOver(playerWon: boolean) {
         revealAnswer();
+        if(playerWon){
+            // do some victory animation
+        }
     }
 
     const answerWeight = unitConversions(props.question.weight);
     const [inputUnit, setInputUnit] = useState<Units>("lbs");
     const [answerHidden, setAnswerHidden] = useState(true);
+    const [answerString, setAnswerString] = useState("");
 
     return (
         <>
@@ -45,7 +50,7 @@ export function Game(props: {question: Question}) {
                     </div>
                     <p className={styles.QuestionName}>{props.question.name}</p>
                 </div>
-                <p hidden={answerHidden} className={styles.QuestionAnswer}>{`The answer was ${answerWeight[inputUnit].toFixed(2)} ${inputUnit} !`}</p>
+                <p hidden={answerHidden} className={styles.QuestionAnswer}>{answerString}</p>
                 <Guess answer={answerWeight} onUnitsChanged={UnitsChanged} onGameOver={handleGameOver}/>
             </div>
         </>
